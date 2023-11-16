5f2d05fc4ef81d40266ebf21ca32a39206c318ba
Anderson Leite <andersonleite.dev@gmail.com>
2023-11-15 22:39:47 -0300

    feat: add method findBYId and refactor list

--- src/main/java/academy/devdojo/springboot/controller/AnimesController.java
+++ src/main/java/academy/devdojo/springboot/controller/AnimesController.java
@@ -1,10 +1,14 @@
 package academy.devdojo.springboot.controller;
 
+import static org.springframework.http.HttpStatus.OK;
+
 import academy.devdojo.springboot.domain.Anime;
 import academy.devdojo.springboot.service.AnimesService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +22,13 @@ public class AnimesController {
     private final AnimesService animesService;
 
     @GetMapping
-    public List<Anime> list(){
-        return animesService.animeListAll();
+    public ResponseEntity<List<Anime>> list(){
+        return new ResponseEntity<>(animesService.animeListAll(), OK);
+    }
+
+    @GetMapping(path = "{id}")
+    public ResponseEntity<Anime> findById(@PathVariable Long id) {
+        return new ResponseEntity<>(animesService.findById(id), OK);
     }
 
 }
--- src/main/java/academy/devdojo/springboot/service/AnimesService.java
+++ src/main/java/academy/devdojo/springboot/service/AnimesService.java
@@ -2,11 +2,23 @@ package academy.devdojo.springboot.service;
 
 import academy.devdojo.springboot.domain.Anime;
 import java.util.List;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class AnimesService {
+
+  private final List<Anime> animeList = List.of(new Anime(1L, "Boku No Hero"), new Anime(2L, "Berserk"));
+
   public List<Anime> animeListAll() {
-    return List.of(new Anime(1L, "Boku No Hero"), new Anime(2L, "Berserk"));
+    return animeList;
+  }
+
+  public Anime findById(Long id) {
+    return animeList.stream()
+        .filter(anime -> anime.getId().equals(id))
+        .findFirst()
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
   }
 }
